755c78a42951d42e0924aa45415a4c8d4f96fa97
martinabongiovanni <martina.bongiovanni06@gmail.com>
2025-03-31 11:10:47 +0200

    eliminato la classe iscrizione_DAO e definito i metodi getIscrizioneDAO e iscrizioneDAO in corso_DAO.

--- database/corso_DAO.py
+++ database/corso_DAO.py
@@ -47,3 +47,41 @@ class corso_DAO:
             self._listaCorsiDelloStudente.append(c)
         cnx.close()
         return self._listaCorsiDelloStudente
+
+    def getIscrizioneDAO(self, matricola, codins):
+        '''
+        Ricerca l'iscrizione di uno studente a un corso
+        :param matricola: matricola di uno studente
+        :param codins: codice di un corso
+        :return:
+        False se non trova un'iscrizione, quindi posso inserirne una nuova
+        True se trova un'iscrizione, quindi lo studente è già iscritto al corso
+        '''
+        cnx = self.dbConnect.get_connection()
+        cursor = cnx.cursor()
+        query = """
+                    SELECT i.*
+                    FROM iscrizione i
+                    WHERE i.matricola = %s AND i.codins = %s"""
+        cursor.execute(query, (matricola, codins))
+        row = cursor.fetchall()
+        if len(row) == 0:
+            return False
+        else:
+            return True
+
+    def iscrizioneDAO(self, matricola, codins):
+        '''
+        Inserisce una nuova iscrizione di uno studente a un corso
+        :param matricola: matricola di uno studente
+        :param codins: codice di un corso
+        :return: tupla di matricola dello studente e codice del corso
+        '''
+        cnx = self.dbConnect.get_connection()
+        cursor = cnx.cursor()
+        query = """
+                    INSERT INTO iscrizione
+                    VALUES (%s, %s)"""
+        cursor.execute(query, (matricola, codins))
+        cnx.commit()
+        return (matricola, codins)
--- model/model.py
+++ model/model.py
@@ -1,5 +1,4 @@
 from database.corso_DAO import corso_DAO
-from database.iscrizione_DAO import iscrizione_DAO
 from database.studente_DAO import studente_DAO
 
 
@@ -8,7 +7,6 @@ class Model:
     def __init__(self):
         self._corsoDAO = corso_DAO()
         self._studenteDAO = studente_DAO()
-        self._iscrizioneDAO = iscrizione_DAO()
 
     def getCorsiDalDAO(self):
         return self._corsoDAO.getCorsi()
@@ -23,7 +21,7 @@ class Model:
         return self._corsoDAO.getCorsiDelloStudente(matricola)
 
     def getIscrizioneDelloStudenteAlCorsoDalDAO(self, matricola, codins):
-        return self._iscrizioneDAO.getIscrizioneDAO(matricola, codins)
+        return self._corsoDAO.getIscrizioneDAO(matricola, codins)
 
     def iscrizioneStudenteACorsoInDAO(self, matricola, codins):
-        return self._iscrizioneDAO.iscrizioneDAO(matricola, codins)
+        return self._corsoDAO.iscrizioneDAO(matricola, codins)
